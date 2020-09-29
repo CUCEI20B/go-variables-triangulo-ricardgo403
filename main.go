1009999dcd838550297cb7b3a787de902e25d1a9
Ricardo Joel Gonzalez Velasco <70970994+ricardgo403@users.noreply.github.com>
2020-09-29 11:44:06 -0500

    Prueba 1

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var base uint64
+	var altura uint64
+	fmt.Scan(&base)
+	fmt.Scan(&altura)
+	area := (base * altura) / 2
+	fmt.Print(area)
 }
